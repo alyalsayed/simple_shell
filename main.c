5d25f6a570e22990d1beb709d3f53b35015305f9
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-04-20 17:43:18 +0000

    readline.c and main.c done

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Program entry point
  * @argc: Arguments count
  * @argv: Arguments array
+ * @envp: Environment variable
  * Return: 0 on success or 1 if error encountered
-*/
-int main(int argc, char **argv)
+ */
+int main(int argc, char **argv, char **envp)
 {
-/* code ... */
+	int ac = (argc - 1), i = 0;
+	char **av;
+	if (argc > 1)
+	{
+		av = malloc(sizeof(char *) * ac);
+		checkalloc(av);
+		while (i < argc)
+		{
+			av[i] = argv[i + 1];
+			i++;
+		}
+		av[i] = NULL;
+		exec(av, envp);
+	}
+	else
+		launch_shell(envp);
+
+	return (0);
 }
