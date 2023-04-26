db9e6ec40ea9d557d0ef4f31681b11bc2730112d
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:13:44 +0200

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -1,14 +1,17 @@
 #include <stddef.h>
 
 /**
- * _strlen - Get the lenght of a string
- * @s: The string
+ * _strlen - get the length of a string
+ * @s: the string
+ *
  * Return: the size of the string
-*/
+ */
 size_t _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 		i++;
-	return ((size_t)i);
+
+	return (size_t)i;
 }
