1a40e13bb9b893f40c20142bbbf5cc139f8a4af8
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:08:49 +0200

    Update readline.c

--- readline.c
+++ readline.c
@@ -3,21 +3,24 @@
 #include "main.h"
 
 /**
- * readline - Read from stdin
- * Return: String on success
+ * readline - read from stdin
+ *
+ * Return: string on success
  */
 char *readline(void)
 {
-size_t length = 0;
-__ssize_t read;
-char *command;
-read = getline(&command, &length, stdin);
-if (read == -1 && feof(stdin))
-	exit(EXIT_SUCCESS);
-else if (read == -1)
-{
-	perror("Error during line reading.");
-	exit(EXIT_FAILURE);
-}
-return (command);
+	size_t length = 0;
+	__ssize_t read;
+	char *command;
+
+	read = getline(&command, &length, stdin);
+	if (read == -1 && feof(stdin))
+		exit(EXIT_SUCCESS);
+	else if (read == -1)
+	{
+		perror("Error during line reading.");
+		exit(EXIT_FAILURE);
+	}
+
+	return (command);
 }
