cd050e6682730f5f7707566bf91ef5195ba274db
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:10:45 +0200

    Update main.h

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 extern char **environ;
+
 int _strcmp(char *__s1, char *__s2);
 int _atoi(char *s);
 size_t _strlen(char *s);
@@ -15,11 +16,12 @@ void exec(char **commands);
 void (*get_built_in_function(char *))(char **);
 void exit_shell(char **args);
 void print_env(char **args);
+
 /**
  * struct built_in_command - Built-in command
  * @name: Name of the command
  * @function: The function pointer to execute
- * description: A struct of functions for built-in shell command
+ * @description: A struct of functions for built-in shell command
 */
 typedef struct built_in_command
 {
