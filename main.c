b9f04b4a3a78e0f8c5dfe6beb59c7a9b16409574
alyalsayed <alyalsayed56@yahoo.com>
2023-04-26 14:30:53 +0200

    fix issues with

--- main.c
+++ main.c
@@ -1,33 +1,44 @@
-#include <stdlib.h>
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - program entry point
- * @argc: arguments count
- * @argv: arguments array
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: 0 on success or 1 if error encountered
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	int ac = (argc - 1);
-	int i = 0;
-	char **av;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	if (argc > 1)
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		av = malloc(sizeof(char *) * ac);
-		checkalloc(av);
-		while (i < argc)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			av[i] = argv[i + 1];
-			i++;
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		av[i] = NULL;
-		exec(av);
+		info->readfd = fd;
 	}
-	else
-		launch_shell();
-
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
