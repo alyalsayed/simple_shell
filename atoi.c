bca4607319542c6e3b0d140036db175e0b230a20
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:02:30 +0200

    fix issues

--- atoi.c
+++ atoi.c
@@ -1,34 +1,39 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer
- * @s: String pointer
- * Return: integer
+ * _atoi - convert a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: the converted integer
  */
 int _atoi(char *s)
 {
-int i = 0, n = 0, sign = 1;
-while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
-{
-if (s[i] == '-')
-{
-sign *= -1;
-}
-i++;
-}
-while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
-{
-if (n >= 0)
-{
-n = n * 10 - (s[i] - '0');
-i++;
-}
-else
-{
-n = n * 10 - (s[i] - '0');
-i++;
-}
-}
-sign *= -1;
-return (n *sign);
+	int i = 0;
+	int num = 0;
+	int sign = 1;
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
+	{
+		if (num >= 0)
+		{
+			num = num * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			num = num * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	sign *= -1;
+
+	return (num * sign);
 }
