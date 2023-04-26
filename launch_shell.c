f3e5c00206628cc8aca55321d62009bb94db7b11
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:09:51 +0200

    Update launch_shell.c

--- launch_shell.c
+++ launch_shell.c
@@ -2,20 +2,21 @@
 #include "main.h"
 
 /**
- * launch_shell - Launch the shell
- * Return: 1 if success or 0 if failed
-*/
+ * launch_shell - launch the shell
+ * Return: void
+ */
 void launch_shell(void)
 {
-char *command;
-char **args;
-while (1)
-{
-	printf("$ ");
-	command = readline();
-	args = split(command);
-	if (args[0] == NULL)
-		continue;
-	exec(args);
-}
+	char *command;
+	char **args;
+
+	while (1)
+	{
+		printf("$ ");
+		command = readline();
+		args = split(command);
+		if (args[0] == NULL)
+			continue;
+		exec(args);
+	}
 }
