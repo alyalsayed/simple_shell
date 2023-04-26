15be20097fa095067de4a2263fe4fc6791c86c4a
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:07:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,29 +2,32 @@
 #include "main.h"
 
 /**
- * main - Program entry point
- * @argc: Arguments count
- * @argv: Arguments array
+ * main - program entry point
+ * @argc: arguments count
+ * @argv: arguments array
+ *
  * Return: 0 on success or 1 if error encountered
  */
 int main(int argc, char **argv)
 {
-int ac = (argc - 1), i = 0;
-char **av;
-if (argc > 1)
-{
-	av = malloc(sizeof(char *) * ac);
-	checkalloc(av);
-	while (i < argc)
-	{
-		av[i] = argv[i + 1];
-		i++;
-	}
-	av[i] = NULL;
-	exec(av);
-}
-else
-	launch_shell();
+	int ac = (argc - 1);
+	int i = 0;
+	char **av;
 
-return (0);
+	if (argc > 1)
+	{
+		av = malloc(sizeof(char *) * ac);
+		checkalloc(av);
+		while (i < argc)
+		{
+			av[i] = argv[i + 1];
+			i++;
+		}
+		av[i] = NULL;
+		exec(av);
+	}
+	else
+		launch_shell();
+
+	return (0);
 }
