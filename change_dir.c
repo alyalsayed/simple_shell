3e2c1173a90c3da084ace0ad3872f995187dae01
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:23:06 +0200

    Update change_dir.c

--- change_dir.c
+++ change_dir.c
@@ -2,11 +2,22 @@
 #include "main.h"
 
 /**
- * change_dir - Change directory
- * @args: Arguments
- * Return: void
-*/
+ * change_dir - change directory
+ * @args: arguments
+ *
+ * Change the current directory to the directory specified in args[1].
+ * If there is an error, an error message is printed.
+ */
 void change_dir(char **args)
 {
+	if (args[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "ash: expected argument to \"cd\"\n");
+	}
+	else if (chdir(args[1]) != 0)
+	{
+		perror("ash");
+	}
 
+	free(args);
 }
