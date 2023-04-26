ac463fb6389866994fdadb11e5e37059c3cdfc7b
Aly Alsayed <84514495+alyalsayed@users.noreply.github.com>
2023-04-26 14:15:47 +0200

    Update strcmp.c

--- strcmp.c
+++ strcmp.c
@@ -1,20 +1,23 @@
 /**
- * _strcmp - Compare two string
- * @__s1: First string
- * @__s2: Second string
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ *
  * Return: 0 if there is a match or 1 if not
-*/
-int _strcmp(char *__s1, char *__s2)
+ */
+int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-while (__s1[i] != '\0' && __s2[i] != '\0')
-{
-	if (__s1[i] != __s2[i])
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (1);
+		i++;
+	}
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	else
 		return (1);
-	i++;
-}
-if (__s1[i] == '\0' && __s2[i] == '\0')
-	return (0);
-else
-	return (1);
 }
