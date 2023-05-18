f61d7b17cce6292a92253101835b55fa819c0c45
Aly Alsayed <alyalsayed56@yahoo.com>
2023-05-18 13:48:28 +0300

    Update exits.c

--- exits.c
+++ exits.c
@@ -4,7 +4,7 @@
  **_strncpy - copies a string
  *@dest: the destination string to be copied to
  *@src: the source string
- *@n: the amount of characters to be copied
+ *@n:the amount of characters to be copied
  *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
