a3188c34d14c907f4689903864f17d8ee5730f1b
Aly Alsayed <alyalsayed56@yahoo.com>
2023-05-18 13:50:16 +0300

    Update history.c

--- history.c
+++ history.c
@@ -4,7 +4,7 @@
  * get_history_file - gets the history file
  * @info: parameter struct
  *
- * Return: allocated string containg history file
+ * Return:allocated string containg history file
  */
 
 char *get_history_file(info_t *info)
